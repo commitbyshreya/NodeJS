f9f77da87d54b2ade8257ab03856a196a1416f9a
shreya <shedgeshreyy03@gmail.com>
2024-06-11 00:27:14 +0530

    broadcaste

--- socketio/index.js
+++ socketio/index.js
@@ -24,29 +24,41 @@ const server = http.createServer(app);
 //attach socket io to http server instance
 const io = socketio(server);
 
+var users = 0;
 //handle socket connection
 io.on("connection", (socket) => {
   console.log("User connected");
+  users++;
 
-//   setTimeout(() => {
-//       // socket.send("Msg sent from server side");
-    //   }, 3000);
-    
-    //CASE 1: sending custom msg from server and catching on client side
-    //   socket.emit('customMsg', {msg:'Server side msg'})
-    
-    //CASE 2: sending custom msg from client side and catching on server side
-    socket.on('customMsg', (data) => {
-        console.log(data)
-    })
-    
-    
+  // socket.emit() -> emit event to only the client who has connected
+  socket.emit("newUserConnect", { message: "Hi , Welcome to the chat" });
+  //socket.broadcast.emit() -> emit event to all connected client except the one who initiated the event
+  socket.broadcast.emit('newUserConnect', {message: users + " Users Connected!"})
+
   //handle disconnection
   socket.on("disconnect", () => {
     console.log("User disconnected");
+    users--;
+    socket.broadcast.emit('newUserConnect', {message: users + " Users Connected!"})
+  //io.sockets.emit() -> emit event to all connected clients
+  // io.sockets.emit("broadcaste", { message: users + " users connected!" });
+    
+    socket.broadcast.emit('newUserConnect', {message: users + " Users Connected!"})
   });
 
   //handle custom event
+
+  //   setTimeout(() => {
+  //       // socket.send("Msg sent from server side");
+  //   }, 3000);
+
+  //CASE 1: sending custom msg from server and catching on client side
+  //   socket.emit('customMsg', {msg:'Server side msg'})
+
+  //CASE 2: sending custom msg from client side and catching on server side
+  socket.on("customMsg", (data) => {
+    console.log(data);
+  });
 });
 
 server.listen(port, () => {
